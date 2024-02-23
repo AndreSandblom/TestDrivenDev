ebffc97aacbfd61ae5fe0635b5c8bc35e9451b84
peishan lu <peishanlugit@gmail.com>
2024-02-23 16:13:25 +0100

    Adding details to all methods

--- game.py
+++ game.py
@@ -3,43 +3,72 @@ from dice import Dice1
 
 class Game:
     def __init__(self, player1=None, player2=None):
-        self.player1 = player1
-        self.player2 = player2
-        self.dice = Dice1
+        if player1 is not None and player2 is not None:
+            self.player1 = player1
+            self.player2 = player2
+        elif player1 is not None:
+            self.player1 = player1
+            # self.player2 = Intelligence()
+        self.dice = Dice1()
+        self.winning_score = 100
         self.score1 = 0
         self.score2 = 0
-    
+
     # A dice is rolled and an integer is returned
     def roll_dice(self):
-        num_rolled = self.dice.roll()
-        return num_rolled
-    
-    # Computer rolls a dice and an integer is returned
-    def intelligence_roll_dice(self, intelligence):
-        self.intelligence = intelligence
-        num_rolled = self.intelligence.roll()                    # Intelligence class rolls a dice
-        return num_rolled
+        return self.dice.roll()
+        
+    # Computer decides if or not to roll a dice and an integer is returned
+    def intelligence_roll_dice(self):
+        return self.intelligence.roll()                    
+
+    # Get the current score of the player
+    def get_score(self, player):
+        if player == self.player1:
+            return self.score1
+        elif player == self.player2:
+            return self.score2
+        else:
+            print("Invalid player") 
 
     # A score adder
-    def add_score(self, score, num_rolled):
-        self.score = score
-        return self.score + num_rolled
+    def increment_score(self, score, num_rolled):
+        if self.has_won(score) is False:
+            return score + num_rolled
+        else:
+            self.winner()
+
+    # Check if a player has won
+    def has_won(self, score):
+        if score >= self.winning_score:
+            return True
+        else:
+            return False
     
+    # Generates a dictionay of name and score
+    def winner(self):
+        winner_table = {self.player1.get_name(): self.score1, self.player2.get_name(): self.score2}
+        return winner_table
+
+    # By setting the winning score to 50 to end game faster
     def cheat(self):
-        pass
-    
-    def restart(self):
-        pass
-
-    def get_score_board(self):
-        pass
-
-    def get_Histogram(self):
-        pass
-
-    def set_score(self, player):
-        player1.set_score()
+        self.winning_score = 50
 
 
-    def quit(self):
-        pass
+    def set_score(self, num1, num2):
+        self.score1 = num1 
+        self.score2 = num2 
+
+
+
+game = Game("A", "B")
+game.set_score(50, 99)
+print(game.get_score("A"))
+print(game.get_score("B"))
+first_roll = game.roll_dice()
+print(game.increment_score(game.get_score("A"), game.roll_dice()))
+print(game.get_score("A"))
+first_roll = game.roll_dice()
+print(game.increment_score(game.get_score("B"), first_roll))
+print(game.get_score("B"))
+
