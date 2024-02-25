e9b6fb638e5c28547f5742136bc251ff1eb6ed70
André Sandblom <159018987+AndreSandblom@users.noreply.github.com>
2024-02-25 17:53:28 +0100

    Add files via upload
    Fixed a couple of typos

---
+++ Player.py
@@ -0,0 +1,29 @@
+
+class Player:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.played_games = 0
+        self.won_games = 0
+
+    def get_player_score(self):
+        print('Player:', self.name)
+        print('Amount of games:', self.played_games)
+        print('Amount of won games:', self.won_games)
+
+    def add_game(self):
+        self.played_games += 1
+
+    def add_win(self):
+        self.won_games += 1
+
+    def reset_games(self):
+        self.played_games = 0
+        self.won_games = 0
+
+    def get_name(self):
+        return self.name
+
+    def change_name(self, new_name):
+        self.name = new_name
+        return self.name
