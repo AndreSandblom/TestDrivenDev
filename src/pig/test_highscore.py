5f6c343488f0970d2513f868bca9d1c08a8a5880
André Sandblom <andre.sandblom0257@stud.hkr.se>
2024-02-28 11:22:50 +0100

    flyttat filer till rätt mapp

---
+++ src/pig/test_highscore.py
@@ -0,0 +1,17 @@
+import unittest
+import highscore
+
+class TestHighscore(unittest.TestCase):
+
+    def test_save_high_score(self):
+        pass
+
+    def test_load_high_score(self):
+        pass
+
+    def test_print_high_score(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/pig/test_player.py
+++ src/pig/test_player.py
@@ -1,5 +1,4 @@
-"""This module contains unit tests for the Player class.
-"""
+"""This module contains unit tests for the Player class."""
 import unittest
 from player import Player
 
@@ -12,29 +11,28 @@ class TestPlayer(unittest.TestCase):
         self.player1 = Player("Player1")
         self.player2 = Player("Player2")
 
-    def tearDown(self) -> None:
-        """Clean up the players after testing."""
-        return super().tearDown()
-
     def test_player_instance(self):
         """Test if the players are instances of the Player class."""
         self.assertIsInstance(self.player1, Player)
         self.assertIsInstance(self.player2, Player)
 
     def test_player_name(self):
+        """Test if the players have the correct names."""
         self.assertEqual(self.player1.name, "Player1")
         self.assertEqual(self.player2.name, "Player2")
 
     def test_player_played_game(self):
+        """Test if the players have played any games."""
         self.assertEqual(self.player1.played_games, 0)
         self.assertEqual(self.player2.played_games, 0)
 
     def test_player_won_game(self):
+        """Test if the players have won any games."""
         self.assertEqual(self.player1.won_games, 0)
         self.assertEqual(self.player2.won_games, 0)
 
     def test_player_add_win(self):
-        
+        """Test if the players can add won games."""
         self.player1.add_win()
         self.assertEqual(self.player1.won_games, 1)
         self.player2.add_win()
@@ -42,6 +40,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player2.won_games, 2)
 
     def test_player_add_game(self):
+        """Test if the players can add played games."""
         self.player1.add_game()
         self.assertEqual(self.player1.played_games, 1)
         self.player2.add_game()
@@ -49,6 +48,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player2.played_games, 2)
 
     def test_player_reset_score(self):
+        """Test if the players can reset their scores."""
         self.player1.add_win()
         self.player1.add_game()
         self.player1.reset_games()
@@ -63,16 +63,19 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player2.won_games, 0)
 
     def test_player_change_name(self):
+        """Test if the players can change their names."""
         self.player1.change_name("PlayerOne")
         self.assertEqual(self.player1.name, "PlayerOne")
         self.player2.change_name("PlayerTwo")
         self.assertEqual(self.player2.name, "PlayerTwo")
 
     def test_player_get_name(self):
+        """Test if the players can get their names."""
         self.assertEqual(self.player1.get_name(), "Player1")
         self.assertEqual(self.player2.get_name(), "Player2")
 
     def test_player_get_score(self):
+        """Test if the players can get their scores."""
         self.assertEqual(
             self.player1.get_player_score(),
             "Player: Player1\nAmount of games: 0\nAmount of won games: 0\n"
