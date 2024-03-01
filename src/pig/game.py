b1df58b9e2afe64e10fd8615c22d297b39db0f04
peishan lu <peishanlugit@gmail.com>
2024-03-01 15:22:29 +0100

    Fixed getter functions for both players.

--- src/pig/game.py
+++ src/pig/game.py
@@ -4,17 +4,14 @@
 
 from dice import Dice
 from player import Player
+from intelligence import Intelligence
 
 class Game:
     
-    def __init__(self, player1=None, player2=None):
+    def __init__(self, player1, player2):
         """Constructor for single and 2 player game."""
-        if player1 is not None and player2 is not None:
-            self.player1 = player1
-            self.player2 = player2
-        elif player1 is not None:
-            self.player1 = player1
-            # self.player2 = Intelligence()
+        self.player1 = player1
+        self.player2 = player2
         self.dice = Dice(6)
         self.winning_score = 100
         self.score1 = 0
@@ -22,20 +19,7 @@ class Game:
 
     def roll_dice(self):
         """A dice is rolled and an integer is returned."""
-        return self.dice.roll()
-        
-    def intelligence_roll_dice(self):
-        """Computer decides if or not to roll a dice and an integer is returned."""
-        pass # return self.intelligence.roll()                    
-
-    # def get_score(self, player):
-    #     """Get the current score of the player."""
-    #     if player == self.player1.get_name():
-    #         return self.score1
-    #     elif player == self.player2.get_name():
-    #         return self.score2
-    #     else:
-    #         raise ValueError("Invalid player.")             # Need to handle error
+        return self.dice.roll()     
         
     def get_score_1(self):
         return self.score1
@@ -43,21 +27,23 @@ class Game:
     def get_score_2(self):
         return self.score2
 
-    def increment_and_determine(self, player, num_rolled):
+    def increment_and_determine_1(self, num_rolled):
         """A score adder that checks if there is a winner,
            if there is, the function returns a dictionary, else 0."""
-        if player == self.player1.get_name():
-            self.score1 += num_rolled
-            if self.has_won(self.score1) is True:
-                return self.winner()
-            else:
-                return 0
-        elif player == self.player2.get_name():
-            self.score2 += num_rolled
-            if self.has_won(self.score2) is True:
-                return self.winner()
-            else:
-                return 0
+        self.score1 += num_rolled
+        if self.has_won(self.score1) is True:
+            return self.winner()
+        else:
+            return 0
+        
+    def increment_and_determine_2(self, num_rolled):
+        """A score adder that checks if there is a winner,
+           if there is, the function returns a dictionary, else 0."""     
+        self.score2 += num_rolled
+        if self.has_won(self.score2) is True:
+            return self.winner()
+        else:
+            return 0
 
     def has_won(self, score):
         """Check if a player has won."""
@@ -69,9 +55,9 @@ class Game:
     def winner(self):
         """Generates a dictionay of name and score."""
         if self.score1 > self.score2:
-            winner_table = {self.player1.get_name(): self.score1, self.player2.get_name(): self.score2}
+            winner_table = {self.player1: self.score1, self.player2: self.score2}
         else:
-            winner_table = {self.player2.get_name(): self.score2, self.player1.get_name(): self.score1}
+            winner_table = {self.player2: self.score2, self.player1: self.score1}
         return winner_table
 
     def cheat(self):
@@ -92,18 +78,14 @@ class Game:
 
 # Print names 
 game = Game("Jenny", "William")
-name1 = game.player1.get_name()
-name2 = game.player2.get_name()
+name1 = game.player1
+name2 = game.player2
 print(name1, name2)
 
 # Set scores
 game.set_score(30, 49)
-print(game.get_score(name1))
-print(game.get_score(name2))
-try:
-    print(game.get_score("A"))
-except ValueError as e:
-    print(e)
+print(game.get_score_1())
+print(game.get_score_2())
 
 # Cheat - winning score = 50
 game.cheat()
@@ -111,18 +93,18 @@ game.cheat()
 # Roll dice and increment score and determine win or not
 first_roll = game.roll_dice()
 print("The first roll is: " + str(first_roll))
-result = game.increment_and_determine(name1, first_roll)
+result = game.increment_and_determine_1(first_roll)
 if  result == 0:
-    print(game.get_score(name1))
+    print(game.get_score_1())
 else:
     print(result)
 
 # Roll dice and increment score and determine win or not
 first_roll = game.roll_dice()
 print("The first roll is: " + str(first_roll))
-result2 = game.increment_and_determine(name2, first_roll)
+result2 = game.increment_and_determine_2(first_roll)
 if  result2 == 0:
-    print(game.get_score(name2))
+    print(game.get_score_2())
 else:
     print(result2)
 
